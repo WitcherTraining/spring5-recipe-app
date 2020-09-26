0e37e9f1a65d111e3f360d021381c05c08da3fbc
WitcherTraining <witcher236@gmail.com>
2020-09-26 22:38:30 +0600

    Implementing findBy methods to Category and UnitOfMeasure repositories

--- src/main/java/kz/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/kz/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,13 +1,34 @@
 package kz.springframework.spring5recipeapp.controllers;
 
+import kz.springframework.spring5recipeapp.domain.Category;
+import kz.springframework.spring5recipeapp.domain.UnitOfMeasure;
+import kz.springframework.spring5recipeapp.repositories.CategoryRepository;
+import kz.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+
+        Optional<Category> optionalCategory = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category ID is: " + optionalCategory.get().getId());
+        System.out.println("Unit of measure is: " + unitOfMeasure.get().getId());
+
         return "index";
     }
 }
--- src/main/java/kz/springframework/spring5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/kz/springframework/spring5recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package kz.springframework.spring5recipeapp.repositories;
 import kz.springframework.spring5recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
